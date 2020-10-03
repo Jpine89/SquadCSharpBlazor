44fb33b48932560e69392361314de1043b5b4755
John Pine <jwmpine@gmail.com>
2020-10-03 15:19:57 -0500

    Pushing changes now

--- SquadCSharpBlazor/Startup.cs
+++ SquadCSharpBlazor/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using SquadCSharpBlazor.Data;
 
 namespace SquadCSharpBlazor
@@ -29,6 +30,7 @@ namespace SquadCSharpBlazor
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddScoped<PlayerListModel>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
