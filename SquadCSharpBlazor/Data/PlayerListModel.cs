5cb9569e5c1ffa14429031ffff95e19607033826
John Pine <jwmpine@gmail.com>
2020-10-03 16:29:11 -0500

    Cleaned Code

--- SquadCSharpBlazor/Data/PlayerListModel.cs
+++ SquadCSharpBlazor/Data/PlayerListModel.cs
@@ -1,24 +1,29 @@
-﻿using System;
+﻿using CoreRCON;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace SquadCSharpBlazor.Data
 {
     public class PlayerListModel
     {
+        private readonly IConfiguration Configuration;
         public string player { get; set; }
 
         public List<string> playerList { get; set; }
 
-        public PlayerListModel()
+        public PlayerListModel(IConfiguration configuration)
         {
+            Configuration = configuration;
             playerList = new List<string>();
         }
 
         public event Action OnChange;
 
-        public void Add(string value)
+        private void Add(string value)
         {
             playerList.Add(value);
             NotifyStateChanged();
@@ -26,5 +31,17 @@ namespace SquadCSharpBlazor.Data
 
         private void NotifyStateChanged() => OnChange?.Invoke();
 
+
+        public async void loadRconAsync()
+        {
+            var rcon = new RCON(IPAddress.Parse(Configuration["IP"]), Convert.ToUInt16(Configuration["Port"]), Configuration["Pass"], 10000, true);
+            await rcon.ConnectAsync();
+            string response = await rcon.SendCommandAsync("ListPlayers");
+            string[] playListString = response.Split("\n");
+            foreach (string value in playListString)
+            {
+                Add(value);
+            }
+        }
     }
 }
