4524ac997f81ffd141c887247bc112b5a3a4d5b1
John Pine <jwmpine@gmail.com>
2021-02-02 21:55:11 -0600

    Commit Changes for BackUp

---
+++ SquadCSharpBlazor/Model/SteamUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SquadCSharpBlazor.Model
+{
+    public class SteamUser
+    {
+        public Int64 steamID { get; set; }
+        public int kills { get; set; }
+        public int deaths { get; set; }
+        public int wounded { get; set; }
+        public int teamkills { get; set; }
+        public int revives { get; set; }
+
+    }
+}
